35cc814e86328298b8d9b781507b315f6520bf73
fOrgeth <ivdovin@mail.ru>
2018-01-23 17:30:45 +0300

    chapter_31 in progress

--- app/src/main/java/com/mcs/th/forge/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/mcs/th/forge/draganddraw/BoxDrawingView.java
@@ -2,6 +2,8 @@ package com.mcs.th.forge.draganddraw;
 
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -9,9 +11,17 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoxDrawingView extends View {
 
     private static final String TAG = "BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
+    private Paint mBoxPaint;
+    private Paint mBackgroungPaint;
 
     public BoxDrawingView(Context context) {
         this(context, null);
@@ -19,6 +29,24 @@ public class BoxDrawingView extends View {
 
     public BoxDrawingView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        mBackgroungPaint = new Paint();
+        mBackgroungPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(mBackgroungPaint);
+        for (Box box : mBoxen) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+            canvas.drawRect(left,top,right,bottom,mBoxPaint);
+        }
     }
 
     @Override
@@ -28,15 +56,23 @@ public class BoxDrawingView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                mCurrentBox = null;
                 break;
         }
 
